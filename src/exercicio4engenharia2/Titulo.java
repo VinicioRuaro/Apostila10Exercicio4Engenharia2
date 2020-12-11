ec34e0afb73ae7e975c2f28886e3398a6885fb3e
VinicioRuaro <50388936+VinicioRuaro@users.noreply.github.com>
2020-12-11 13:48:45 -0300

    Add files via upload

---
+++ src/exercicio4engenharia2/Titulo.java
@@ -0,0 +1,17 @@
+package exercicio4engenharia2;
+
+public class Titulo {
+int prazo;
+public Titulo(int codigo)
+{
+	this.prazo = codigo+1;
+}
+public int getPrazo() {
+	return prazo;
+}
+
+public void setPrazo(int prazo) {
+	this.prazo = prazo;
+}
+
+}
